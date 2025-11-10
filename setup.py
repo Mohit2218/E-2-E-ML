49b88e1ef85dfbeb4d7f21a074003865f12caedf
Mohit Saini <mhtsaini18@gmail.com>
2025-11-10 13:40:08 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E= '-e .'
+def get_requirements(file_path:str)->List[str]:
+   #This function will return the list of requirements
+   requirements=[]
+   with open(file_path) as file_obj:
+      requirements=file_obj.readlines()
+      requirements=[req.replace("\n","") for req in requirements]
+
+      if HYPHEN_E in requirements:
+         requirements.remove(HYPHEN_E)
+    
+   return requirements   
+
+setup(
+name='E-2-E-ML',
+version='0.0.1',
+author='Mohit',
+author_email='mhtsaini18@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
